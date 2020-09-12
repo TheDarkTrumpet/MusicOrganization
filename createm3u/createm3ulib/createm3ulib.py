b2dd3a84e84bb578b3aea41053f8689f243387a6
David Thole <david@thedarktrumpet.com>
2020-09-12 18:51:14 -0500

    Adding more of the search parameters, this now searches properly and calls the save operation pretty well properly, but not perfectly

--- createm3u/createm3ulib/createm3ulib.py
+++ createm3u/createm3ulib/createm3ulib.py
@@ -1,14 +1,36 @@
 __all__ = ["createm3ulib"]
 
 import os
+from os import path
 
 class createm3ulib:
     def __init__(self, basedir):
+        if(path.exists(basedir) == False):
+            raise Exception(RuntimeError, 'Base Directory does not exist')
         self.basedir = basedir
 
     def do(self):
+        current_dir = ''
+        found_files = []
         for subdir, dirs, files in os.walk(self.basedir):
             for file in files:
+                parent_dir = self.get_parentdir(subdir)
+                if(current_dir == ''):
+                    current_dir = parent_dir
+                #print("Current Dir: " + current_dir + " Parent Dir: " + parent_dir)
+                if(current_dir != parent_dir):
+                    self.save_m3u(current_dir, found_files)
+                    found_files = []
+                    current_dir = parent_dir
+
+                #print("Subdir: " + subdir)
                 # print os.path.join(subdir, file)
                 filepath = subdir + os.sep + file
-                print(filepath)
+                found_files.append(filepath)
+                # print(filepath)
+
+    def get_parentdir(self, directory):
+        return '/'.join(directory.split('/')[0:-1])
+
+    def save_m3u(self, basedir, files):
+        print("In save operation - basedir: " + basedir + " - Length: " + str(len(files)))
