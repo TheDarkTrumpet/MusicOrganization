922ea1894681f4582d70799d34aa861a7541b0f7
David Thole <david@thedarktrumpet.com>
2020-09-12 16:23:01 -0500

    Updating gitignore and creating a base directory for my implementation

---
+++ createm3u/__main__.py
@@ -0,0 +1,23 @@
+"""
+main.py
+Main entry when executing createm3u as a 'program'
+
+Use: python createm3u <base_path>
+"""
+
+import click
+
+def print_help(ctx, opts, args):
+    if args is False:
+        return
+    click.echo(ctx.get_help())
+    ctx.exit()
+
+@click.command()
+@click.argument('basedir', help="Base directory to start search from")
+@click.pass_context
+def main(basedir):
+    pass
+
+if __name__ == '__main__':
+    main()
