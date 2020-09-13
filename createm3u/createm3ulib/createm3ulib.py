1b2508bb29577e12b7d0fd98c092b1725da283d9
David Thole <david@thedarktrumpet.com>
2020-09-12 19:17:31 -0500

    removing some comments

--- createm3u/createm3ulib/createm3ulib.py
+++ createm3u/createm3ulib/createm3ulib.py
@@ -20,20 +20,15 @@ class createm3ulib:
                 parent_dir = self.get_parentdir(subdir)
                 if(len(found_files) == 0 and parent_dir != current_dir):
                     current_dir = parent_dir
-                #print("Current Dir: " + current_dir + " Parent Dir: " + parent_dir)
                 if(current_dir != parent_dir):
                     self.save_m3u(current_dir, found_files)
                     found_files = []
                     current_dir = parent_dir
-
-                #print("Subdir: " + subdir)
-                # print os.path.join(subdir, file)
                 filepath = subdir + os.sep + file
                 file_ext = self.get_extension(filepath)
 
                 if(file_ext not in self.file_exclude and file not in self.file_exclude):
                     found_files.append(filepath)
-                # print(filepath)
 
     def get_parentdir(self, directory):
         return '/'.join(directory.split('/')[0:-1])
